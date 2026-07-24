815ef1fbded0cc6ae7aaa0978c149d41185cdbc0
Shatha Ammar <shathaammar407@gmail.com>
2026-07-24 21:58:07 +0300

    Add Entity Framework Core and database context

---
+++ FleetMaintenance.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using FleetMaintenance.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FleetMaintenance.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+
+        public DbSet<MaintenanceType> MaintenanceTypes { get; set; }
+
+        public DbSet<MaintenanceRecord> MaintenanceRecords { get; set; }
+    }
+}
