dc35952b2198985b25f4b0bdb84e1205c8931432
Shatha Ammar <shathaammar407@gmail.com>
2026-08-18 22:01:06 +0300

    Add maintenance record paged query

--- FleetMaintenance.Application/Interfaces/Repositories/IMaintenanceRecordRepository.cs
+++ FleetMaintenance.Application/Interfaces/Repositories/IMaintenanceRecordRepository.cs
@@ -1,4 +1,6 @@
-﻿using FleetMaintenance.Domain.Entities;
+﻿using FleetMaintenance.Application.Common.Models;
+using FleetMaintenance.Application.DTOs.MaintenanceRecords;
+using FleetMaintenance.Domain.Entities;
 
 namespace FleetMaintenance.Application.Interfaces.Repositories;
 
@@ -6,6 +8,7 @@ public interface IMaintenanceRecordRepository
     : IGenericRepository<MaintenanceRecord>
 {
     Task<List<MaintenanceRecord>> GetAllWithDetailsAsync();
+    Task<PagedResult<MaintenanceRecord>> GetPagedAsync(MaintenanceRecordFilterDto filter);
 
     Task<MaintenanceRecord?> GetByIdWithDetailsAsync(int id);
 
--- FleetMaintenance.Infrastructure/Repositories/MaintenanceRecordRepository.cs
+++ FleetMaintenance.Infrastructure/Repositories/MaintenanceRecordRepository.cs
@@ -1,4 +1,6 @@
-﻿using FleetMaintenance.Application.Interfaces.Repositories;
+﻿using FleetMaintenance.Application.Common.Models;
+using FleetMaintenance.Application.DTOs.MaintenanceRecords;
+using FleetMaintenance.Application.Interfaces.Repositories;
 using FleetMaintenance.Domain.Entities;
 using FleetMaintenance.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -21,6 +23,76 @@ public class MaintenanceRecordRepository: GenericRepository<MaintenanceRecord>, 
             .Include(record => record.MaintenanceType)
             .OrderByDescending(record => record.ScheduledDate)
             .ToListAsync();
+    }
+
+    public async Task<PagedResult<MaintenanceRecord>> GetPagedAsync(MaintenanceRecordFilterDto filter)
+    {
+        IQueryable<MaintenanceRecord> query =
+            Context.MaintenanceRecords
+                .AsNoTracking()
+                .Include(record => record.Vehicle)
+                .Include(record => record.MaintenanceType);
+
+        if (!string.IsNullOrWhiteSpace(filter.Search))
+        {
+            string search = filter.Search.Trim();
+
+            query = query.Where(record =>
+                record.Vehicle.PlateNumber.Contains(search) ||
+                record.MaintenanceType.Name.Contains(search));
+        }
+
+        if (filter.VehicleId.HasValue)
+        {
+            query = query.Where(record =>
+                record.VehicleId == filter.VehicleId.Value);
+        }
+
+        if (filter.MaintenanceTypeId.HasValue)
+        {
+            query = query.Where(record =>
+                record.MaintenanceTypeId ==
+                filter.MaintenanceTypeId.Value);
+        }
+
+        if (filter.Status.HasValue)
+        {
+            query = query.Where(record =>
+                record.Status == filter.Status.Value);
+        }
+
+        if (filter.FromDate.HasValue)
+        {
+            DateTime fromDate = filter.FromDate.Value.Date;
+
+            query = query.Where(record =>
+                record.ScheduledDate >= fromDate);
+        }
+
+        if (filter.ToDate.HasValue)
+        {
+            DateTime toDateExclusive =
+                filter.ToDate.Value.Date.AddDays(1);
+
+            query = query.Where(record =>
+                record.ScheduledDate < toDateExclusive);
+        }
+
+        int totalCount = await query.CountAsync();
+
+        var records = await query
+            .OrderByDescending(record => record.ScheduledDate)
+            .Skip((filter.PageNumber - 1) * filter.PageSize)
+            .Take(filter.PageSize)
+            .ToListAsync();
+
+        return new PagedResult<MaintenanceRecord>
+        {
+            Items = records,
+            PageNumber = filter.PageNumber,
+            PageSize = filter.PageSize,
+            TotalCount = totalCount
+        };
     }
 
     public async Task<MaintenanceRecord?> GetByIdWithDetailsAsync(int id)
