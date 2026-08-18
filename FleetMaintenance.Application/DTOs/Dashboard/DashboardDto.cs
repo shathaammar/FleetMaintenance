1a29b891f8ffdb1558ecf13c9bf789adbdcd6141
Shatha Ammar <shathaammar407@gmail.com>
2026-08-18 21:10:51 +0300

    Add dashboard DTOs

---
+++ FleetMaintenance.Application/DTOs/Dashboard/DashboardDto.cs
@@ -0,0 +1,22 @@
+﻿namespace FleetMaintenance.Application.DTOs.Dashboard;
+
+public class DashboardDto
+{
+    public int TotalVehicles { get; set; }
+
+    public int ActiveVehicles { get; set; }
+
+    public int VehiclesInMaintenance { get; set; }
+
+    public int OutOfServiceVehicles { get; set; }
+
+    public int ScheduledMaintenances { get; set; }
+
+    public int OverdueMaintenances { get; set; }
+
+    public int CompletedMaintenances { get; set; }
+
+    public decimal TotalMaintenanceCost { get; set; }
+
+    public List<UpcomingMaintenanceDto> UpcomingMaintenances { get; set; } = new();
+}
---
+++ FleetMaintenance.Application/DTOs/Dashboard/UpcomingMaintenanceDto.cs
@@ -0,0 +1,16 @@
+﻿namespace FleetMaintenance.Application.DTOs.Dashboard;
+
+public class UpcomingMaintenanceDto
+{
+    public int MaintenanceRecordId { get; set; }
+
+    public int VehicleId { get; set; }
+
+    public string PlateNumber { get; set; } = string.Empty;
+
+    public string MaintenanceTypeName { get; set; } = string.Empty;
+
+    public DateTime ScheduledDate { get; set; }
+
+    public int? DueMileage { get; set; }
+}
