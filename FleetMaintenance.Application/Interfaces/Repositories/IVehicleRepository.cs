54d96e294305052209f58ea85c30e3af01139817
Shatha Ammar <shathaammar407@gmail.com>
2026-08-18 21:41:35 +0300

    Add vehicle search filtering and pagination query

--- FleetMaintenance.Application/Interfaces/Repositories/IVehicleRepository.cs
+++ FleetMaintenance.Application/Interfaces/Repositories/IVehicleRepository.cs
@@ -1,9 +1,12 @@
-﻿using FleetMaintenance.Domain.Entities;
+﻿using FleetMaintenance.Application.Common.Models;
+using FleetMaintenance.Application.DTOs.Vehicles;
+using FleetMaintenance.Domain.Entities;
 
 namespace FleetMaintenance.Application.Interfaces.Repositories
 {
     public interface IVehicleRepository : IGenericRepository<Vehicle>
     {
         Task<bool> PlateNumberExistsAsync(string plateNumber, int? excludedVehicleId = null);
+        Task<PagedResult<Vehicle>> GetPagedAsync(VehicleFilterDto filter);
     }
 }
--- FleetMaintenance.Infrastructure/Repositories/VehicleRepository.cs
+++ FleetMaintenance.Infrastructure/Repositories/VehicleRepository.cs
@@ -1,4 +1,6 @@
-﻿using FleetMaintenance.Application.Interfaces.Repositories;
+﻿using FleetMaintenance.Application.Common.Models;
+using FleetMaintenance.Application.DTOs.Vehicles;
+using FleetMaintenance.Application.Interfaces.Repositories;
 using FleetMaintenance.Domain.Entities;
 using FleetMaintenance.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -31,5 +33,43 @@ public class VehicleRepository: GenericRepository<Vehicle>, IVehicleRepository
             vehicle.PlateNumber.ToLower() == normalizedPlateNumber &&
             (!excludedVehicleId.HasValue ||
              vehicle.Id != excludedVehicleId.Value));
+    }
+
+    public async Task<PagedResult<Vehicle>> GetPagedAsync(VehicleFilterDto filter)
+    {
+        IQueryable<Vehicle> query = Context.Vehicles
+            .AsNoTracking();
+
+        if (!string.IsNullOrWhiteSpace(filter.Search))
+        {
+            string search = filter.Search.Trim();
+
+            query = query.Where(vehicle =>
+                vehicle.PlateNumber.Contains(search) ||
+                vehicle.Make.Contains(search) ||
+                vehicle.Model.Contains(search));
+        }
+
+        if (filter.Status.HasValue)
+        {
+            query = query.Where(vehicle =>
+                vehicle.Status == filter.Status.Value);
+        }
+
+        int totalCount = await query.CountAsync();
+
+        var vehicles = await query
+            .OrderByDescending(vehicle => vehicle.CreatedAt)
+            .Skip((filter.PageNumber - 1) * filter.PageSize)
+            .Take(filter.PageSize)
+            .ToListAsync();
+
+        return new PagedResult<Vehicle>
+        {
+            Items = vehicles,
+            PageNumber = filter.PageNumber,
+            PageSize = filter.PageSize,
+            TotalCount = totalCount
+        };
     }
 }
